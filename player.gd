6b7fa198865d85dec2c3e8e2cae3a12b8c29b0f4
Jame <Jamesferris447@gmail.com>
2026-06-13 12:36:42 +1000

    removed jumping
    that wont be needed hehehehe

--- player.gd
+++ player.gd
@@ -1,15 +1,11 @@
 extends CharacterBody2D
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -510
 
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 		
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
