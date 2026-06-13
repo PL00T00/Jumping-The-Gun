cef6720ae4e2c9170280909d4fcdbb0cd15186cb
Jame <Jamesferris447@gmail.com>
2026-06-13 14:05:06 +1000

    Added time out for shoot
    Removed requirement of being on the ground to shoot, now there must be a 1 second break between shots instead

--- player.gd
+++ player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
 const SPEED = 400.0
-const SHOOT_UP_FORCE = -300.0
+const SHOOT_UP_FORCE = -800.0
+const SHOOT_TIMEOUT: float = 1.0
+
+var can_shoot: bool = true
 
 func _physics_process(delta: float) -> void:
 	
@@ -13,8 +16,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 20)
-	if is_on_floor():
-		if Input.is_action_just_pressed("Shoot1"):
-			velocity.y = SHOOT_UP_FORCE
-		
+	if Input.is_action_just_pressed("Shoot1") and can_shoot:
+		shoot()
 	move_and_slide()
+
+func shoot():
+	velocity.y += SHOOT_UP_FORCE
+	can_shoot = false
+	await get_tree().create_timer(SHOOT_TIMEOUT).timeout
+	can_shoot = true
+	
