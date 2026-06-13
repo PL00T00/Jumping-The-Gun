b48ca02d3298d8a33e589e48f4eb8fcb991a5ea0
Jame <Jamesferris447@gmail.com>
2026-06-13 13:57:03 +1000

    added basic shoot mechanic
    currently only send you upwards no matter where your mouse is and you must be on the ground to use it

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 400.0
+const SHOOT_UP_FORCE = -300.0
 
 func _physics_process(delta: float) -> void:
 	
@@ -12,5 +13,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 20)
+	if is_on_floor():
+		if Input.is_action_just_pressed("Shoot1"):
+			velocity.y = SHOOT_UP_FORCE
 		
 	move_and_slide()
