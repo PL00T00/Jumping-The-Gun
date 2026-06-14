77615e38ea645be1fe9b1ba3718b5fe77dc00ed1
Jame <Jamesferris447@gmail.com>
2026-06-14 20:07:29 +1000

    reworked movement
    Completely reworked movement to make it feel a shittt ton better and less clanky

--- player.gd
+++ player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
-const SHOOT_FORCE = -800.0
+const SPEED = 500.0
+const GROUND_ACCELERATION = 4000.0
+const AIR_ACCELERATION = 2000.0
+const FRICTION = 35000.0
 
-var can_shoot: bool = true
-var is_recoiling: bool = false
+const SHOOT_FORCE = 1100.0
+const RELOAD_TIME = 0.35
+
+var can_shoot = true
 
 func _physics_process(delta: float) -> void:
 	
@@ -12,22 +16,33 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		
 	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		if not is_recoiling:
-			velocity.x = move_toward(velocity.x, 0, 20)
+	
+	if direction != 0:
+		var accelerartion = GROUND_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+		velocity.x = move_toward(
+			velocity.x,
+			direction * SPEED,
+			accelerartion * delta
+		)
+	elif is_on_floor():
+		velocity.x = move_toward(
+			velocity.x,
+			0,
+			FRICTION * delta
+		)
+		
 	if Input.is_action_just_pressed("Shoot1") and can_shoot:
 		shoot()
 	move_and_slide()
 
 func shoot():
-	var shoot_direction = global_position.direction_to(get_global_mouse_position())
-	velocity = shoot_direction * SHOOT_FORCE
+	var shoot_direction = global_position.direction_to(
+		get_global_mouse_position()
+	)
+	
+	velocity -= shoot_direction * SHOOT_FORCE
 	can_shoot = false
-	is_recoiling = true
-	await get_tree().create_timer(0.5).timeout
-	is_recoiling = false
-	await get_tree().create_timer(0.85).timeout
+	
+	await get_tree().create_timer(RELOAD_TIME).timeout
 	can_shoot = true
 	
