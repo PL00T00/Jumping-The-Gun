6ee496532933d42dbe89efe43a1f30bc95bc3c3e
Jame <Jamesferris447@gmail.com>
2026-06-15 16:55:13 +1000

    fixed movement
    Still feels too heavy for some reaosn, also need to figureo ut recoil better

--- player.gd
+++ player.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
 const SPEED = 500.0
-const WALK_ACCELERATION = 2500.0
-const MAX_WALK_SPEED = 500.0
 const GROUND_ACCELERATION = 4000.0
 const AIR_ACCELERATION = 1500.0
-const FRICTION = 35000.0
+const FRICTION = 1500.0
 
-const SHOOT_FORCE = 750.0
+const SHOOT_FORCE = 850.0
 const RELOAD_TIME = 1
 
 var can_shoot = true
@@ -15,20 +13,17 @@ var can_shoot = true
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.3
 		
 	var direction := Input.get_axis("Left", "Right")
 	
 	if direction != 0:
-		velocity.x += direction * WALK_ACCELERATION * delta
-	
-	if abs(velocity.x) < MAX_WALK_SPEED:
-		velocity.x = clamp(
+		var acceleration = GROUND_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+		velocity.x = move_toward(
 			velocity.x,
-			-MAX_WALK_SPEED,
-			MAX_WALK_SPEED
+			direction * SPEED,
+			acceleration * delta
 		)
-
 	elif is_on_floor():
 		velocity.x = move_toward(
 			velocity.x,
